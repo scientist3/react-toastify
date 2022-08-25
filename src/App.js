e1865e355e29b78b4b154f37a33af0b2330ac8f3
Aamir Sofi <eraamirsofi@gmail.com>
2022-08-26 00:09:29 +0530

    Working Taostfy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const notify = () => {
+		toast("Wow so easy!");
+		toast.success("🦄 Wow so easy!", {
+			position: "top-center",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+		});
+	};
 
+	return (
+		<div>
+			<button onClick={notify}>Notify!</button>
+			<a href="https://fkhadra.github.io/react-toastify/introduction/">Demo</a>
+			<ToastContainer />
+		</div>
+	);
+}
 export default App;
